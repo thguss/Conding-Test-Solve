4be3eb4f7c503deecf0d8b712c9df17ee9de8191
thguss <thgus345@naver.com>
2022-09-16 01:52:46 +0900

    [LEVEL1] 나머지가 1이 되는 수 찾기

---
+++ 22-09-16/Solution12.java
@@ -0,0 +1,10 @@
+class Solution {
+  public int solution(int n) {
+    int answer = 1;
+      
+      while(true) {
+          if (n%answer == 1) return answer;
+          answer += 1;
+      }
+  }
+}
