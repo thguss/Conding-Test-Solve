961df0b50c9af6beed286f05460ef323a162c88c
thguss <thgus345@naver.com>
2022-09-16 23:34:21 +0900

    [LEVEL1] 최소직사각형

---
+++ 22-09-17/Solution15.java
@@ -0,0 +1,23 @@
+
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int clen = sizes.length; int rlen = sizes.length;
+        
+        for (int i = 0; i < clen; i++) {
+            if (sizes[i][0] > sizes[i][1]) {
+                int temp = sizes[i][0];
+                sizes[i][0] = sizes[i][1];
+                sizes[i][1] = temp;
+            }
+        }
+        
+        int cMax = 0; int rMax = 0;
+        for (int i = 0; i < clen; i++) {
+            cMax = Math.max(cMax, sizes[i][0]);
+            rMax = Math.max(rMax, sizes[i][1]);
+        }
+        
+        return cMax * rMax;
+    }
+}
