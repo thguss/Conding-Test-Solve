75985f30cd0d14a940cc7ccbba4521e2d4d61793
thguss <thgus345@naver.com>
2022-09-16 20:24:28 +0900

    [LEVEL1] 같은 숫자는 싫어 - FIX : 문제명 수정

--- 22-09-17/Solution9.java
+++ 22-09-17/Solution9.java
@@ -2,11 +2,11 @@ import java.util.*;
 
 public class Solution9 {
     public int[] solution(int []arr) {
-        ArrayList<Integer> al = new ArrayList<Integer>();
+      ArrayList<Integer> al = new ArrayList<Integer>();
+      int len = arr.length;
         
         al.add(arr[0]);
         
-        int len = arr.length;
         for (int i = 1; i < len; i++) {
             if (arr[i] != arr[i-1]) al.add(arr[i]);
         }
